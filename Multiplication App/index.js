c6dde54bb8c3447c76b485f376cc8c141a2d1e3d
Victoriakerubo <victoriakerubog@gmail.com>
2023-12-14 16:42:05 +0300

    commit

--- Multiplication App/index.js
+++ Multiplication App/index.js
@@ -1,4 +1,4 @@
-const num1 = Math.ceil(Math.random() * 10);
+const num1 = Math.ceil(Math.random() * 20);
 const num2 = Math.ceil(Math.random() * 10);
 
 const questionE1 = document.getElementById("question");
